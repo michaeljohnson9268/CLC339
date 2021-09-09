18a9cfdb227e4ba493a56317a7df38e575f10df7
michaeljohnson9268 <michaeljohnson9268@gmail.com>
2021-09-09 10:18:38 -0700

    Test

---
+++ topic1/src/main/java/michael/com/gcu/Topic1Application.java
@@ -0,0 +1,15 @@
+package michael.com.gcu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Topic1Application {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		
+		SpringApplication.run(Topic1Application.class, args);
+	}
+
+}
---
+++ topic1/src/test/java/michael/com/gcu/Topic1ApplicationTests.java
@@ -0,0 +1,13 @@
+package michael.com.gcu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Topic1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
