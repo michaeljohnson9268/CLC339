da5ee9de9a52e8bcea625c99a8966cae6668ab90
michaeljohnson9268 <75800336+michaeljohnson9268@users.noreply.github.com>
2021-10-02 15:42:22 -0700

    Add files via upload

---
+++ workspaceCST-339/example9.16/src/main/java/com/gcu/controller/Application.java
@@ -0,0 +1,15 @@
+package com.gcu.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan({ "com.gcu" })
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
